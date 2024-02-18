1839b9c1d30e271ed0376bc8d4830fb62acb5605
Harry Hanskat <harryhanskat13@gmail.com>
2024-02-18 10:31:19 -0600

    reinstall

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
+        <p>{!data ? "Loading...": data}</p>
       </header>
     </div>
   );
