483674e802f3c384aa7b183468c4d11769e79d73
Harry Hanskat <harryhanskat13@gmail.com>
2024-03-04 08:34:29 -0600

    Fixed Proxy issue. DB Connected and Post from Postman working.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const path = require('path');
 const express = require("express");
 const { Pool } = require('pg');
+const { v4: uuidv4 } = require('uuid');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
 
 const PORT = process.env.PORT || 3001;
+const publicDirectory = path.resolve(__dirname, '../client/build');
+
+let liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicDirectory);
 
 const app = express();
+
+app.use(connectLiveReload());
+
+app.use(express.static(publicDirectory));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 // Postgres Database setup
 const pool = new Pool({
@@ -13,9 +28,6 @@ const pool = new Pool({
   database: 'groovemeister',
   port: 5432
 });
-
-// Have Node serve the files for our build React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/test', async (req, res) => {
   try {
@@ -29,8 +41,12 @@ app.get('/test', async (req, res) => {
   }
 });
 
+app.get('/uuid', (req, res) => {
+  res.json(uuidv4());
+})
+
 // Get first row of the practice items table.
-app.get('/db', async (req, res) => {
+app.get('/api/PracticeItem', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM practice_items WHERE id = 1');
@@ -42,9 +58,19 @@ app.get('/db', async (req, res) => {
   }
 });
 
+app.post('/api/addPracticeItem', (req, res) => {
+  try {
+    console.log(req.body);
+    res.status(200).send("Practice Item Added");
+  } catch (err) {
+    console.error('Not implemented yet', err);
+    res.status(500).send('Not implemented yet');
+  }
+});
+
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server! Live reloaded! Live reload?" });
 });
 
 // All other GET requests not handled before will return our React app
@@ -52,6 +78,20 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const start = async() => {
+  // Test Database connection before starting the server
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+    console.log("DB Connection Successful", result.rows[0]);
+
+    // Given a successful connection then we start the server.
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error connecting to database: ', err);
+  }
+};
+
+start();
